8e7f670000c27997f54e2c8ed72d8b84ae02c969
shintakuix <candeiasva@gmail.com>
2019-07-29 14:16:06 -0400

    full app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger1', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger2', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger3', true);
+
